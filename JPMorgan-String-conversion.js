20faef58cc0997aef0c01151ed41d7cbf84ae873
JosephSCha <josephshincha@gmail.com>
2020-09-25 19:10:13 -0400

    string conversion

--- JPMorgan-String-conversion.js
+++ JPMorgan-String-conversion.js
@@ -1,1 +1,13 @@
 // Capitalize first letter of each word in a string. 
+
+function capitalizeFirstLetter(string) {
+    splitStr = string.split(" ")
+    for (i=0; i < splitStr.length; i++){
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1)
+    }
+    return splitStr.join(" ")
+}
+
+var testStr = "hello world!"
+
+console.log(capitalizeFirstLetter(testStr))
